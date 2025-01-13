999f7c7ea09ada864ada7378aa18bc2eec26115a
Blaz Orazem <blaz@orazem.si>
2025-01-13 10:18:34 +0100

    Changes

--- updates/create_numencode_widgets_highlights_items_table.php
+++ updates/create_numencode_widgets_highlights_items_table.php
@@ -19,7 +19,7 @@ class CreateNumencodeWidgetsHighlightsItemsTable extends Migration
             $table->string('link_title')->nullable();
             $table->string('picture')->nullable();
             $table->boolean('is_published');
-            $table->integer('sort_order');
+            $table->integer('sort_order')->nullable();
             $table->timestamps();
 
             $table->foreign('group_id')->references('id')->on('numencode_widgets_highlights_groups')->onDelete('cascade');
